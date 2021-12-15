805f4181bc56bc7e4d96ea7d6039951927870969
viettd56 <viet751993@gmail.com>
2021-12-15 11:57:09 +0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -26,6 +26,11 @@ module.exports = {
           position: 'left',
           label: 'Docs',
         },
+         {
+          href: 'https://chainverse.org/',
+          label: 'Website',
+          position: 'left',
+        },
       ],
     },
     footer: {
@@ -94,7 +99,7 @@ module.exports = {
             'https://github.com/facebook/docusaurus/edit/main/website/blog/',
         },
         theme: {
-          customCss: require.resolve('./src/css/styles.css'),
+          customCss: require.resolve('./src/css/custom.css'),
         },
       },
     ],
