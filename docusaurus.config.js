271c887f8417dc660229d4f11a8855ef11f1d04c
Đặng Đình Khánh <khanhdd@appota.com>
2021-12-15 12:06:52 +0700

    UI: Edit style

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -99,7 +99,7 @@ module.exports = {
             'https://github.com/facebook/docusaurus/edit/main/website/blog/',
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve('./src/css/styles.css'),
         },
       },
     ],
